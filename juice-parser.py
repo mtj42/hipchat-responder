31990a02d52c63fd245d229157feaf939f266cb9
thejuiceman <markjudice@gmail.com>
2015-10-28 17:51:41 -0500

    Changed a few small things. Attempted error handling for failed HTTP(S) connections.

--- juice-parser.py
+++ juice-parser.py
@@ -1,5 +1,5 @@
 """
-Script to deal with co-workers constantly @mentionig you in a HipChat room.
+Script to deal with co-workers constantly @mentioning you in a HipChat room.
 
 Grabs the last 5 messages in a room and checks for any @mentions; if you are mentioned, choose and send a response accordingly.
 """
@@ -43,20 +43,23 @@ def send_response():
 old_msg_ids = []
 
 while True:
-    r = requests.get("{}v2/room/{}/history/latest?max-results=5&auth_token={}".format(base, room_id, token))
-    messages = r.json()['items']
-    with open('juice_parser.json', 'w') as outfile:
-        outfile.write(json.dumps(messages))
-    # print(json.dumps(messages))
-    for msg in messages:
-        if msg['mentions'] != []:
-            for mention in msg['mentions']:
-                if mention['name'] == 'Mark Judice':
-                    if msg['id'] not in old_msg_ids:
-                        # print(msg['message'])
-                        send_response()
-                    old_msg_ids.append(msg['id'])
-
+    try:
+        r = requests.get("{}v2/room/{}/history/latest?max-results=5&auth_token={}".format(base, room_id, token))
+        messages = r.json()['items']
+        with open('juice_parser.json', 'w') as outfile:
+            outfile.write(json.dumps(messages))
+        # print(json.dumps(messages))
+        for msg in messages:
+            if msg['mentions'] != []:
+                for mention in msg['mentions']:
+                    if mention['name'] == 'Mark Judice':
+                        if msg['id'] not in old_msg_ids:
+                            # print(msg['message'])
+                            send_response()
+                        old_msg_ids.append(msg['id'])
+    except: 
+        print("Exception caught... continue")
+        pass
 
     print('\n---\n')
     time.sleep(random.randint(5,12)) # Random sleep before responses
